a35648d73f5cacdc4265bc0b2ccc23bc4a2ef603
Faishol Nuris Sholihin 21-076 <107903310+Faishol95@users.noreply.github.com>
2022-08-30 20:15:38 +0700

    Add files via upload

---
+++ fungsi 3 parameer.php
@@ -0,0 +1,11 @@
+<?php 
+	function profil($nama, $nim, $asal){
+		echo ("<br>Nama : ".$nama);
+		echo ("<br>NIM  : ".$nim);
+		echo ("<br>Asal : ".$asal."<br>");
+	}
+	profil("Faishol Nuris Sholihin","210411100076","Pamekasan");
+	profil("Roy Suwanda" ,"210411100046","Ponorogo");
+	profil("Moch. Miftachur Rifqi Al Husain","210411100125","Lamongan");
+?>
+	
